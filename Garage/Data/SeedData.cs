45b4d2afc66c900c113749180205a21b25978806
Camila Paschini <camila.paschini@gmail.com>
2026-01-16 10:59:46 +0100

    more cleanup

--- Garage/Data/SeedData.cs
+++ Garage/Data/SeedData.cs
@@ -2,21 +2,21 @@ using Garage.Constants;
 using Garage.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Garage.Data;
 
 public class SeedData
 {
+    private static GarageContext _context = default!;
     private static UserManager<ApplicationUser> _userManager = default!;
     private static RoleManager<IdentityRole> _roleManager = default!;
 
     public static async Task Initialize(GarageContext context, IServiceProvider services)
     {
+        _context = context;
         _roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
         _userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-
-        if (context.Roles.Any())
-            return;
 
         List<string> defaultRoles = new()
         {
@@ -28,13 +28,15 @@ public class SeedData
 
         var usersToAdd = SeededUsers.Default;
 
-        await SeedUsers(usersToAdd);
-        await AssignRoles(usersToAdd);
-        await AddFullNameClaims(usersToAdd);
+        await SeededUsers.SeedUsers(usersToAdd, _userManager);
+        await SeededUsers.AssignRoles(usersToAdd, _userManager);
+        await SeededUsers.AddFullNameClaims(usersToAdd, _userManager);
     }
 
     private static async Task SeedRoles(List<string> roles)
     {
+        if (_context.Roles.Any()) return;
+
         foreach (var role in roles)
         {
             if (await _roleManager.RoleExistsAsync(role))
@@ -43,52 +45,6 @@ public class SeedData
             var result = await _roleManager.CreateAsync(new IdentityRole(role));
             if (!result.Succeeded)
                 throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
-        }
-    }
-
-    private static async Task SeedUsers(IEnumerable<SeedUser> users)
-    {
-        foreach (var seed in users)
-        {
-            var userFound = await _userManager.FindByEmailAsync(seed.User.Email);
-            if (userFound == null)
-            {
-                userFound = seed.User;
-
-                var result = await _userManager.CreateAsync(userFound, seed.Password);
-                if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
-            }
-        }
-    }
-
-    private static async Task AssignRoles(IEnumerable<SeedUser> userRoles)
-    {
-        foreach (var seed in userRoles)
-        {
-            var userFound = await _userManager.FindByEmailAsync(seed.User.Email);
-            var isInRole = await _userManager.IsInRoleAsync(userFound, seed.Role);
-
-            if (!isInRole)
-            {
-                var result = await _userManager.AddToRoleAsync(userFound, seed.Role);
-                if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
-            }
-        }
-    }
-
-    private static async Task AddFullNameClaims(IEnumerable<SeedUser> users)
-    {
-        foreach (var seed in users)
-        {
-            var userFound = await _userManager.FindByEmailAsync(seed.User.Email);
-            var fullName = $"{userFound.FirstName} {userFound.LastName}";
-            var hasClaim = (await _userManager.GetClaimsAsync(userFound))
-                .Any(c => c.Type == "FullName" && c.Value == fullName);
-            if (!hasClaim)
-            {
-                var claimResult = await _userManager.AddClaimAsync(userFound, new System.Security.Claims.Claim("FullName", fullName));
-                if (!claimResult.Succeeded) throw new Exception(string.Join("\n", claimResult.Errors));
-            }
         }
     }
 }
--- Garage/Data/SeededUsers.cs
+++ Garage/Data/SeededUsers.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Garage.Constants;
 using Garage.Models;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace Garage.Data;
 
@@ -37,4 +41,55 @@ public static class SeededUsers
             UserRoles.Member
         )
     };
+
+    public static async Task SeedUsers(IEnumerable<SeedUser> users, UserManager<ApplicationUser> userManager)
+    {
+        foreach (var seed in users)
+        {
+            var userFound = await userManager.FindByEmailAsync(seed.User.Email);
+            if (userFound == null)
+            {
+                userFound = seed.User;
+                var result = await userManager.CreateAsync(userFound, seed.Password);
+                if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors.Select(e => e.Description)));
+            }
+        }
+    }
+
+    public static async Task AssignRoles(IEnumerable<SeedUser> userRoles, UserManager<ApplicationUser> userManager)
+    {
+        foreach (var seed in userRoles)
+        {
+            var userFound = await userManager.FindByEmailAsync(seed.User.Email);
+            if (userFound == null)
+                continue;
+
+            var isInRole = await userManager.IsInRoleAsync(userFound, seed.Role);
+            if (!isInRole)
+            {
+                var result = await userManager.AddToRoleAsync(userFound, seed.Role);
+                if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors.Select(e => e.Description)));
+            }
+        }
+    }
+
+    public static async Task AddFullNameClaims(IEnumerable<SeedUser> users, UserManager<ApplicationUser> userManager)
+    {
+        foreach (var seed in users)
+        {
+            var userFound = await userManager.FindByEmailAsync(seed.User.Email);
+            if (userFound == null)
+                continue;
+
+            var fullName = $"{userFound.FirstName} {userFound.LastName}";
+            var hasClaim = (await userManager.GetClaimsAsync(userFound))
+                .Any(c => c.Type == "FullName" && c.Value == fullName);
+
+            if (!hasClaim)
+            {
+                var claimResult = await userManager.AddClaimAsync(userFound, new System.Security.Claims.Claim("FullName", fullName));
+                if (!claimResult.Succeeded) throw new Exception(string.Join("\n", claimResult.Errors.Select(e => e.Description)));
+            }
+        }
+    }
 }

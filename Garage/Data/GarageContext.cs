df688a910a5cb48cc396ae31d4a8916f697ef1ab
Mikael Nordman <mikael.nordman147@gmail.com>
2026-01-16 09:52:17 +0100

    Added migration

--- Garage/Data/GarageContext.cs
+++ Garage/Data/GarageContext.cs
@@ -24,66 +24,66 @@ public class GarageContext(DbContextOptions<GarageContext> options) : IdentityDb
     // Make sure the Ids are unique and start from 1
     // it will be used for testing purposes creating initial data in the database only once.
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+    //    base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<ParkedVehicle>().HasData(
-    new ParkedVehicle
-    {
-        Id = 1,
-        VehicleType = VehicleType.CAR,
-        Registration = "ABC123",
-        Color = "Red",
-        Brand = "Volvo",
-        Model = "XC60",
-        Wheels = 4,
-        ParkTime = new DateTime(2025, 12, 17, 9, 30, 0)
-    },
-    new ParkedVehicle
-    {
-        Id = 2,
-        VehicleType = VehicleType.MOTORCYCLE,
-        Registration = "MOTO77",
-        Color = "Black",
-        Brand = "Yamaha",
-        Model = "MT-07",
-        Wheels = 2,
-        ParkTime = new DateTime(2025, 12, 17, 10, 15, 0)
-    },
-    new ParkedVehicle
-    {
-        Id = 3,
-        VehicleType = VehicleType.ATV,
-        Registration = "ATV999",
-        Color = "Green",
-        Brand = "Polaris",
-        Model = "Sportsman",
-        Wheels = 4,
-        ParkTime = new DateTime(2025, 12, 17, 11, 0, 0)
-    },
-    new ParkedVehicle
-    {
-        Id = 4,
-        VehicleType = VehicleType.BUS,
-        Registration = "BUS001",
-        Color = "White",
-        Brand = "Scania",
-        Model = "Citywide",
-        Wheels = 6,
-        ParkTime = new DateTime(2025, 12, 17, 8, 45, 0)
-    },
-    new ParkedVehicle
-    {
-        Id = 5,
-        VehicleType = VehicleType.TRUCK,
-        Registration = "TRK888",
-        Color = "Blue",
-        Brand = "MAN",
-        Model = "TGX",
-        Wheels = 8,
-        ParkTime = new DateTime(2025, 12, 17, 7, 20, 0)
-    }
-         );
-    }
+    //    modelBuilder.Entity<ParkedVehicle>().HasData(
+    //new ParkedVehicle
+    //{
+    //    Id = 1,
+    //    VehicleType = VehicleType.CAR,
+    //    Registration = "ABC123",
+    //    Color = "Red",
+    //    Brand = "Volvo",
+    //    Model = "XC60",
+    //    Wheels = 4,
+    //    ParkTime = new DateTime(2025, 12, 17, 9, 30, 0)
+    //},
+    //new ParkedVehicle
+    //{
+    //    Id = 2,
+    //    VehicleType = VehicleType.MOTORCYCLE,
+    //    Registration = "MOTO77",
+    //    Color = "Black",
+    //    Brand = "Yamaha",
+    //    Model = "MT-07",
+    //    Wheels = 2,
+    //    ParkTime = new DateTime(2025, 12, 17, 10, 15, 0)
+    //},
+    //new ParkedVehicle
+    //{
+    //    Id = 3,
+    //    VehicleType = VehicleType.ATV,
+    //    Registration = "ATV999",
+    //    Color = "Green",
+    //    Brand = "Polaris",
+    //    Model = "Sportsman",
+    //    Wheels = 4,
+    //    ParkTime = new DateTime(2025, 12, 17, 11, 0, 0)
+    //},
+    //new ParkedVehicle
+    //{
+    //    Id = 4,
+    //    VehicleType = VehicleType.BUS,
+    //    Registration = "BUS001",
+    //    Color = "White",
+    //    Brand = "Scania",
+    //    Model = "Citywide",
+    //    Wheels = 6,
+    //    ParkTime = new DateTime(2025, 12, 17, 8, 45, 0)
+    //},
+    //new ParkedVehicle
+    //{
+    //    Id = 5,
+    //    VehicleType = VehicleType.TRUCK,
+    //    Registration = "TRK888",
+    //    Color = "Blue",
+    //    Brand = "MAN",
+    //    Model = "TGX",
+    //    Wheels = 8,
+    //    ParkTime = new DateTime(2025, 12, 17, 7, 20, 0)
+    //}
+    //     );
+    //}
 }
---
+++ Garage/Migrations/20260116084951_ParkingSpots.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Garage.Migrations
+{
+    /// <inheritdoc />
+    public partial class ParkingSpots : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.CreateTable(
+                name: "ParkingSpots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParkedVehicleID = table.Column<int>(type: "int", nullable: true),
+                    ParkingSpotSize = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Blocked = table.Column<bool>(type: "bit", nullable: false),
+                    ParkTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParkingSpots", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ParkingSpots_ParkedVehicle_ParkedVehicleID",
+                        column: x => x.ParkedVehicleID,
+                        principalTable: "ParkedVehicle",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParkingSpots_Blocked_ParkedVehicleID",
+                table: "ParkingSpots",
+                columns: new[] { "Blocked", "ParkedVehicleID" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParkingSpots_ParkedVehicleID",
+                table: "ParkingSpots",
+                column: "ParkedVehicleID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParkingSpots_ParkTime",
+                table: "ParkingSpots",
+                column: "ParkTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ParkingSpots");
+
+            migrationBuilder.InsertData(
+                table: "ParkedVehicle",
+                columns: new[] { "Id", "Brand", "Color", "Model", "ParkTime", "Registration", "VehicleType", "Wheels" },
+                values: new object[,]
+                {
+                    { 1, "Volvo", "Red", "XC60", new DateTime(2025, 12, 17, 9, 30, 0, 0, DateTimeKind.Unspecified), "ABC123", 0, 4 },
+                    { 2, "Yamaha", "Black", "MT-07", new DateTime(2025, 12, 17, 10, 15, 0, 0, DateTimeKind.Unspecified), "MOTO77", 1, 2 },
+                    { 3, "Polaris", "Green", "Sportsman", new DateTime(2025, 12, 17, 11, 0, 0, 0, DateTimeKind.Unspecified), "ATV999", 2, 4 },
+                    { 4, "Scania", "White", "Citywide", new DateTime(2025, 12, 17, 8, 45, 0, 0, DateTimeKind.Unspecified), "BUS001", 3, 6 },
+                    { 5, "MAN", "Blue", "TGX", new DateTime(2025, 12, 17, 7, 20, 0, 0, DateTimeKind.Unspecified), "TRK888", 4, 8 }
+                });
+        }
+    }
+}

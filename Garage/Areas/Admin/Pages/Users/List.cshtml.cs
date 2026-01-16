5c531a9531334fd124bed79cf1b2b16352cc2823
Olle Mattsson <rymdolle@users.noreply.github.com>
2026-01-16 12:29:37 +0100

    User list search/filter

--- Garage/Areas/Admin/Pages/Users/List.cshtml.cs
+++ Garage/Areas/Admin/Pages/Users/List.cshtml.cs
@@ -1,50 +1,69 @@
-using Garage.Data;
+#nullable disable
 using Garage.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Garage.Areas.Admin.Pages.Users;
 
 public class ListModel : PageModel
 {
-    private readonly GarageContext _context;
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
 
-    public ListModel(GarageContext context, UserManager<ApplicationUser> userManager)
+    public ListModel(
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager)
     {
-        _context = context;
         _userManager = userManager;
+        _roleManager = roleManager;
     }
 
     public IList<UserModel> Users { get; set; } = new List<UserModel>();
+    public SelectList Roles { get; set; }
+    public string Query { get; set; }
+    public string RoleFilter { get; set; }
 
     public class UserModel
     {
-        public string Id { get; set; }
+        public required string Id { get; set; }
         public required string Name { get; set; }
-        public required IEnumerable<string> Roles { get; set; }
-        public int VehicleCount { get; set; }
-        public int TotalRevenue { get; set; }
+        public required int VehicleCount { get; set; }
+        public required int TotalRevenue { get; set; }
     }
 
-    public async Task<IActionResult> OnGetAsync()
+    /// <summary>
+    /// List users
+    /// </summary>
+    /// <param name="q">Query</param>
+    /// <param name="role">RoleFilter</param>
+    /// <returns></returns>
+    public async Task<IActionResult> OnGetAsync(string q, string role)
     {
-        var users = await _userManager.Users.ToListAsync();
-        foreach (var user in users)
+        var query = _userManager.Users;
+        if (role != null)
+            query = _userManager.GetUsersInRoleAsync(role).Result.AsQueryable();
+        if (q != null)
+            query = query.Where(s => s.UserName!.Contains(q));
+
+        var users = query.Select(s => new UserModel
         {
-            var roles = await _userManager.GetRolesAsync(user);
-            Users.Add(new UserModel
-            {
-                Id = user.Id,
-                Name = user.UserName ?? string.Empty,
-                Roles = roles,
-                VehicleCount = 0, // TODO: Add count from context
-                TotalRevenue = 0, // TODO: Add revenue from context
-            });
-        }
+            Id = s.Id,
+            Name = s.UserName ?? string.Empty,
+            VehicleCount = 0, // TODO: Add count from context
+            TotalRevenue = 0, // TODO: Add revenue from context
+        }).ToList();
+
+        var roles = _roleManager.Roles
+            .Select(s => new SelectListItem(s.Name, s.Name))
+            .ToList();
+        Roles = new SelectList(roles, "Value", "Text");
+        Users = users;
+        Query = q;
+        RoleFilter = role;
 
         return Page();
     }
+
 }
